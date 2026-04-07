51f6499f16134a52eea8ea55f0f4104b589f76dc
deanenettles <deane@deanenettles.com>
2026-04-07 12:58:11 -0400

    change contact.php

--- contact.php
+++ contact.php
@@ -170,7 +170,8 @@ $csrfToken = generateCsrfToken();
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/ocs.css">
+  <link rel="stylesheet" href="css/styles.css">
+
   <style>
 body {background-color:#ccc;}
 

425ec569ba513e76784a5e910b90b244df29c2f4
deanenettles <deane@deanenettles.com>
2026-04-07 12:41:39 -0400

    add portfolio.css

--- contact.php
+++ contact.php
@@ -171,6 +171,8 @@ $csrfToken = generateCsrfToken();
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/ocs.css">
+  <link rel="stylesheet" href="css/portfolio.css">
+
   <style>
     .form-message {
       padding: 15px;
